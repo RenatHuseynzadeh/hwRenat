22144af489b2a8a7aab2b329e95db69146f71a89
RenatHuseynzadeh <124487433+RenatHuseynzadeh@users.noreply.github.com>
2023-07-21 09:12:23 +0300

    hwRenat

---
+++ Bom2Dz.js
@@ -0,0 +1,43 @@
+// 1
+// предупреждение,потверждение,окно запроса
+
+// 2
+// alert('Осторожно')
+
+// 3
+// confirm('lubis bors?')
+
+// 4
+// prompt('lubis bors?')
+
+// 5
+// setTimeout(function, milliseconds)
+
+// 6
+// setInterval(function, milliseconds)
+
+// 7
+// setTimeout(() => {
+    // console.log('НАЧАЛО')
+// 
+// }, 1000)
+
+// 8
+// setInterval(() => {
+    //  console.log('ПОВТОР');
+//  
+//  }, 2000)
+
+// 9
+// куки может хранить данные о пользователе в небольших текстовых файлах
+
+// 10
+// document.cookie = "username=Renat"
+
+// 11
+// просто пишем ровно и меняем
+
+// 12
+// нужно добавить уже прошедшую дату
+//  document.cookie = "username=Renat; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/"
+
---
+++ Bom2zadaniya.js
@@ -0,0 +1,32 @@
+// 1
+
+//  function two(numm){
+    //  for(let i in numm){
+        //  console.log(`"${numm[i]}": "${numm[i].toUpperCase()}"`);
+    //  }  
+//  }
+//  
+//  two(['p', 's'])
+
+// 4
+//  let obj1 = {name: 'Max', age: 25, wor: 'school'};
+//  let obj2 = {name: 'Max', age: 25};
+//   
+//  
+//  
+//  if(Object.keys(obj1).length === Object.keys(obj2).length){
+    //  console.log('verno');
+//  } else if(Object.keys(obj1).length !== Object.keys(obj2).length){
+    //  console.log('ne verno');
+//  }
+
+// 5
+//  function two(orders, cost){
+    //   for(let i in orders){
+        //   if(orders[i]>cost){
+            //  console.log(orders[i]);
+        //   }
+    //   }  
+//  }
+//  
+//  two({a: 3000, b: 200, c: 1050}, 1000)
